02a054782d4932063f746a60fc0801738e9fa47d
FabianFlo <fa.flores@duocuc.cl>
2025-12-23 21:13:32 -0300

    Se agrega GitHub Pages deployment workflow

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/Portfolio/', // Reemplaza con el nombre exacto de tu repo
+    build: {
+        outDir: 'dist'
+    }
+})
