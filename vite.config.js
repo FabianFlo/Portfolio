85d5dc50a199ade9a5fcc9b02ed575000ce7d641
FabianFlo <fa.flores@duocuc.cl>
2025-12-29 11:00:36 -0300

    Actualizar el resultado de la compilación y el flujo de trabajo de implementación
    Añade recursos e imágenes compilados al directorio dist, actualiza el flujo de trabajo de implementación de GitHub Pages para Vite y mejora index.html para usar Vite y Tailwind mediante PostCSS. También actualiza .gitignore para ignorar dist y la salida de compilación relacionada.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/Portfolio/', // Reemplaza con el nombre exacto de tu repo
-    build: {
-        outDir: 'dist'
-    }
-})
+    base: "/Portfolio/",
+});
